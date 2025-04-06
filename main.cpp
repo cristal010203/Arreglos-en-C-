c355ff5de7d28caae0b2f7d49e298042e9570b4b
Cmunoz <cmunozm6@miumg.edu.gt>
2025-04-05 21:50:17 -0600

    Se crea el procedimiento buscar contacto, en donde se recorren los arrays del nombre y numero para encontrar el contacto con cualquiera de los valores asignados que tiene en estos arreglos

--- main.cpp
+++ main.cpp
@@ -41,6 +41,37 @@ void agregarContacto() {
     totalContactos++;
     cout << "Contacto agregado.\n";
 }
+// Mostrar todos los contactos
+void mostrarContactos() {
+    cout << "\n--- Lista de Contactos ---\n";
+    if (totalContactos == 0) {
+        cout << "No hay contactos.\n";
+        return;
+    }
+
+    for (int i = 0; i < totalContactos; i++) {
+        cout << i + 1 << ". " << nombres[i] << " - " << telefonos[i] << endl;
+    }
+}
+// Buscar un contacto por nombre o teléfono
+void buscarContacto() {
+    string buscar;
+    bool encontrado = false;
+
+    cout << "Buscar por nombre o telefono: ";
+    getline(cin, buscar);
+
+    for (int i = 0; i < totalContactos; i++) {
+        if (nombres[i].find(buscar) != string::npos || telefonos[i].find(buscar) != string::npos) {
+            cout << "Encontrado: " << nombres[i] << " - " << telefonos[i] << endl;
+            encontrado = true;
+        }
+    }
+
+    if (!encontrado) {
+        cout << "Contacto no encontrado.\n";
+    }
+}
 
 
 // Menú principal
@@ -53,19 +84,22 @@ int main() {
     do {
         cout << "\n=== Agenda de Contactos ===\n";
         cout << "1. Agregar contacto\n";
+        cout << "2. Mostrar contactos\n";
+        cout << "3. Buscar contacto\n";
 
         cin >> opcion;
         getline(cin, dummy); // Limpiar el buffer
 
         switch (opcion) {
             case 1: agregarContacto(); break;
-           
+            case 2: mostrarContactos(); break;
+            case 3: buscarContacto(); break;
+
             default: cout << "Opcion no valida.\n"; break;
         }
 
     } while (opcion != 6);
 
     return 0;
-
 
 }
